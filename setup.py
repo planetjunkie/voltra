5dd306dec0012b2923b4e21f21340fdb797cc86b
planetjunkie <alshhh.123.987@gmail.com>
2022-05-24 07:58:34 +0400

    uwu

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+print("Welcome to the voltra setup!")
+
+projectdata = {
+    "name": "Voltra",
+    "version": "0.1 Public RC",
+    "description": "Voltra is a tool for helping users to use template file structures for thier projects.",
+    "author": "Ali Alshamsi",
+    "license": "MIT",
+    "packages": ["voltra"],
+}
+
+## create setup script using setup tools
+
