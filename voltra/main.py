efea0cce2eeca9bac70b5dfe9d83cc87afd5972e
planetjunkie <alshhh.123.987@gmail.com>
2022-05-25 09:00:30 +0400

    Version 0.0.5 PRC Code

--- voltra/main.py
+++ voltra/main.py
@@ -1,8 +1,7 @@
 import argparse
 import os
 from venv import create
-
-## This is a temporary fix excuse me :(
+import time
 
 def main():
     parser = argparse.ArgumentParser(description='Voltra: A tool made to help you create a new project structure.')
@@ -17,20 +16,84 @@ def main():
 
 def create_file():
     print("Create a new project structure.")
-    lang = input("What Language would you like to use? (Supported: Python) ")
-
-    if lang == "Python" or "python":
-        ("Language supported! Answer the following questions")
-        proname = input("What is the name of your project? (e.g. MyProject) ")
-        saveloc = input("Where would you like to create your project? (e.g. /home/user/Desktop/MyProject) ")
-
+    language = input("What Language would you like to use? (Supported: Python, C) > ")
+    proname = input("What is the name of your project? (e.g. MyProject) > ")
+    saveloc = input("Where would you like to create your project? (e.g. /home/user/Desktop/MyProject) > ")
+    ## The loading is intentional to show the user that the program is working.
+    
+    def py():
         print("Creating project structure...")
+        print("Loading ......")
+        time.sleep(1)
+        print("Loading |.....")
+        time.sleep(1)
+        print("Loading ||....")
+        time.sleep(1)
+        print("Loading |||...")
+        time.sleep(1)
+        print("Loading ||||..")
+        time.sleep(1)
+        print("Loading |||||.")
+        time.sleep(1)
+        print("Loading ||||||")
+        time.sleep(1)
+
+
         os.mkdir(saveloc + "/" + proname)
         os.mkdir(saveloc + "/" + proname + "/" + proname)
         os.mkdir(saveloc + "/" + proname + "/" + "test")
 
+    def C_lang():
+        print("Creating project structure...")
+        print("Loading ......")
+        time.sleep(1)
+        print("Loading |.....")
+        time.sleep(1)
+        print("Loading ||....")
+        time.sleep(1)
+        print("Loading |||...")
+        time.sleep(1)
+        print("Loading ||||..")
+        time.sleep(1)
+        print("Loading |||||.")
+        time.sleep(1)
+        print("Loading ||||||")
+        time.sleep(1)
+
+        ##this is a mess :3
+        os.mkdir(saveloc + "/" + proname)
+        os.mkdir(saveloc + "/" + proname     + "/" + "src")
+        os.mkdir(saveloc + "/" + proname + "/" + "test")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "debug")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "release")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "debug" + "/" + "lib")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "release" + "/" + "lib")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "debug" + "/" + "include")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "release" + "/" + "include")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "debug" + "/" + "src")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "release" + "/" + "src")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "debug" + "/" + "test")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "release" + "/" + "test")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "debug" + "/" + "test" + "/" + "src")
+        os.mkdir(saveloc + "/" + proname + "/" + "tools" + "/" + "build" + "/" + "linux" + "/" + "x64" + "/" + "release" + "/" + "test" + "/" + "src")
+        
+    if language == "Python" or language == "python" or language == "PYTHON" or language == "Python3" or language == "python3" or language == "PYTHON3" or language == "py" or language == "Py" or language == "PY":
+        print("Python Language Template coming in!")
+        time.sleep(0.1)
+        py()
+    elif language == "C" or language == "c" or language == "C++" or language == "c++":
+        print("C/C++ Language Template coming in!")
+        time.sleep(0.1)
+        C_lang()
     else:
-        print("UhOh! Language not supported! Please try again. (Tip: run voltra -C To try again")
+        print("Sorry, that language is not supported yet.")
+        time.sleep(2)
+        os.system("clear")
+        create_file()   
 
 
 
